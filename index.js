7ed4d947a59083668ab0ee1d93ce21905fc25ad4
Anibca <38027956+Anibca@users.noreply.github.com>
2022-02-18 16:23:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function increment() {
+console.log("btn-clicked") 
+}
+let count = 5
+
+let countEl = document.getElementById("count-el")
+  console.log(countEl)
+  
+ let count = 0
+ function increment()
+ {
+   
+ count = count + 1
+ countEl.innerText = count
+ }  
+ function save(){
+ console.log(count)
+ }
+ 
